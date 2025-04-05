657a175e2e50e4e9c0403dd62ccc8c7a251fd646
Risiamu <kousakiaya@outlook.com>
2025-04-05 21:33:43 +0200

    ADD: create initial Typst document structure in batch script

--- create_typst_rpd.bat
+++ create_typst_rpd.bat
@@ -3,3 +3,14 @@ mkdir temp_project
 cd temp_project
 git init
 git submodule add https://github.com/Risiamu/Risi-s-Professional-Doc.git
+
+(
+echo #import "Risi-s-Professional-Doc\lib.typ": ^*
+echo.
+echo #show: doc =^> risi-pro^(
+echo   title: "My Document",
+echo   authors: ^("John Doe"^),
+echo   date: datetime.today^(^),
+echo   doc
+echo ^)
+) > main.typ
